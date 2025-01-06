a2982d9122355a002ae3996e7603330d87a845cb
xycabcd <145047708+xycabcd@users.noreply.github.com>
2025-01-06 18:59:24 -0500

    Ensure FCGIExcption privacy

--- jfcgihelper/FCGIException.java
+++ jfcgihelper/FCGIException.java
@@ -1,12 +1,12 @@
 package jfcgihelper;
 public final class FCGIException extends RuntimeException {
     private int errorNum;
-    public FCGIException(int n) {
+    FCGIException(int n) {
         if(n == 0) throw new IllegalArgumentException("invalid error number: 0");
         super();
         errorNum = n;
     }
-    public FCGIException(int n, String message) {
+    FCGIException(int n, String message) {
         if(n == 0) throw new IllegalArgumentException("invalid error number: 0");
         super(message);
         errorNum = n;
