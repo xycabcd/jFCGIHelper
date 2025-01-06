86bc0339a4378be884229b9952529c949503b5af
xycabcd <145047708+xycabcd@users.noreply.github.com>
2025-01-06 15:36:57 -0500

    add the unadded FCGIException.java

---
+++ com/fastcgi/FCGIException.java
@@ -0,0 +1,15 @@
+package com.fastcgi;
+public final class FCGIException extends RuntimeException {
+    private int errorNum;
+    public FCGIException(int n) {
+        super();
+        errorNum = n;
+    }
+    public FCGIException(int n, String message) {
+        super(message);
+        errorNum = n;
+    }
+    public int getErrorNumber() {
+        return errorNum;
+    }
+}
