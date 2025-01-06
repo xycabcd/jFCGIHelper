dbb22b000ef80c8a0e2d75506f739219b8cb5750
xycabcd <145047708+xycabcd@users.noreply.github.com>
2025-01-06 18:58:10 -0500

    Ensure error number is valid

--- jfcgihelper/FCGIException.java
+++ jfcgihelper/FCGIException.java
@@ -2,10 +2,12 @@ package jfcgihelper;
 public final class FCGIException extends RuntimeException {
     private int errorNum;
     public FCGIException(int n) {
+        if(n == 0) throw new IllegalArgumentException("invalid error number: 0");
         super();
         errorNum = n;
     }
     public FCGIException(int n, String message) {
+        if(n == 0) throw new IllegalArgumentException("invalid error number: 0");
         super(message);
         errorNum = n;
     }
